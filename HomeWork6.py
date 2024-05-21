e34a086ecca4a7c7518208b17f7868692c09c623
RocknRof1 <168718063+RocknRof1@users.noreply.github.com>
2024-05-21 14:50:48 +0300

    Add files via upload

---
+++ HomeWork6.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 5.5, True, 'Atlas'
+print(immutable_var)
+#immutable_var[3] = False
+
+muttable_var_ = ([1, 5.5, True,'Bivis'])
+print(muttable_var_)
+muttable_var_[2] = False
+print(muttable_var_)
